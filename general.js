171b7947131303837e388f1d4405f7a145ad8547
ShyJajabe <shinyjajabe@outlook.com>
2017-07-20 09:37:24 +0800

    7/20

---
+++ general.js
@@ -0,0 +1,9 @@
+/**
+ * Created by admin on 2017/4/18.
+ */
+String.prototype.replaceTag= function(reallyDo, replaceWith, ignoreCase) {
+    if (!RegExp.prototype.isPrototypeOf(reallyDo)) {
+        return this.replace(new RegExp(reallyDo, (ignoreCase ? "gi": "g")), replaceWith);
+    } else {
+        return this.replace(reallyDo, replaceWith);
+    }};
